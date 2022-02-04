f5507266958f908d1947388d14950a7ad2b53c87
russmaster76 <97500904+russmaster76@users.noreply.github.com>
2022-02-04 13:54:22 -0800

    creating files and tester

---
+++ skilldemoy.java
@@ -0,0 +1,8 @@
+public class skilldemoy {
+    static int sum;
+
+    public static int subtraction(int a, int b) {
+        sum = a - b;
+        return sum;
+    }
+}
---
+++ skilldemoytester.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skilldemoytester {
+    
+    @Test
+    public void subtraction() {
+        assertEquals(3, skilldemoy.subtraction(4, 2));
+    }
+}
